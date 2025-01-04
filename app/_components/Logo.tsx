6ad98f992f0cd07b2c94a7ec386c207925e99a31
codedhola <codedhola2000@gmail.com>
2025-01-04 23:53:44 +0100

    feat: added header

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png";
+
 function Logo() {
   return (
-    <a href="/" className="flex items-center gap-4 z-10">
-      <img height="60" width="60" alt="The Wild Oasis logo" />
+    <Link href="/" className="flex items-center gap-4 z-10">
+      <Image 
+        src={logo}
+        height={60}
+        quality={100}
+        width={60}
+        alt="The Wild Oasis logo"
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
-    </a>
+    </Link>
   );
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import * as React from 'react';
+import "@/app/_styles/globals.css"
+import Header from './_components/Header';
+
+export const metadata = {
+  title: 'Wild Oasis',
+  description: 'Wild Oasis is a platform for connecting people with nature.',
+}
 
 export default function RootLayout({
     children,
@@ -7,7 +14,15 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          
+          <Header />
+          <main>
+            {children}
+
+          </main>
+          
+          </body>
       </html>
     )
   }
