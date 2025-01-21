cc1079ce4cb4213b8623166c1a24cad10b4ed801
codedhola <codedhola2000@gmail.com>
2025-01-21 01:33:42 +0100

    feat: completed the deletion

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
-import { createBooking, updateBooking, updateGuest } from "./data-service";
+import { createBooking, deleteBooking, getBookings, updateBooking, updateGuest } from "./data-service";
 import { redirect } from "next/navigation";
 
 export async function signInAction() {
@@ -73,6 +73,21 @@ export async function createBookingAction(bookingData, formData) {
   revalidatePath(`/cabins/${bookingData.cabinId}`);
 
   redirect("/cabins/thankyou");
+}
+
+export async function deleteBookingAction(bookingId) {
+  const session: any = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  deleteBooking(bookingId)
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signOutAction() {
